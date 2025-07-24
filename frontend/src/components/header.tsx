f1bec097c8eb3775e5a9b742ba12f06f97a97c00
Oatsinthepine <zzy20001201@gmail.com>
2025-07-24 20:07:13 +1000

    updated the Header component with the functionality of toggle icon of dark/light theme change.

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,18 +1,56 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
+
+
 
 const Header = () => {
+
+    const { theme, setTheme } = useTheme();
+    // set the default theme to dark
+    const currentTheme = theme === "dark";
+
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2
         supports-[backdrop-filter]:bg-background/60">
-            <div>
+            <div className="flex items-center justify-between px-4">
+                {/* <Link> here is the client side router */}
                 <Link to={"/"}>
-                <img src="/vite.svg" alt="Climate App Logo" className="h-14 w-10"></img>
+                {/* use ternary operator to change the logo based on the theme */}
+                <img src= {currentTheme ? "/logo.png" : "/logo2.png"}
+                alt="Climate App Logo" className="h-14"></img>
                 </Link>
-            </div>
-            <div>
+
+                <div>
                 {/* search */}
                 {/* theme toggle */}
-            </div>
+                <div
+                    onClick={() => setTheme(currentTheme ? "light" : "dark")}
+                    className="flex items-center cursor-pointer relative h-6 w-6"
+                >
+                    {/* Sun Icon */}
+                    <Sun
+                        className={`
+                            absolute transition-all duration-500
+                            ${currentTheme
+                                ? "opacity-100 rotate-0"
+                                : "opacity-0 rotate-90"}
+                            h-6 w-6 text-yellow-500
+                        `}
+                    />
+                    {/* Moon Icon */}
+                    <Moon
+                        className={`
+                            absolute transition-all duration-500
+                            ${!currentTheme
+                                ? "opacity-100 rotate-0"
+                                : "opacity-0 -rotate-90"}
+                            h-6 w-6 text-gray-500
+                        `}
+                    />
+                </div>
+                </div>
+            </div>        
         </header>
     )
 }
--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -6,11 +6,12 @@ const Layout = ({ children }: PropsWithChildren) => {
         <div className="bg-gradient-to-br from-background to-muted">
             <Header/>
             <main className="min-h-screen conatiner mx-auto px-4 py-8">
-                {children}
+            {/* This {children} is where the routed page appears! */}
+                {children} 
             </main>
             <footer className = "boarder-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
                 <div className="container mx-auto px-4 text-center text-gray-400">
-                    <p>Just another practice project</p>
+                    <p>Just another 🌦️ practice project</p>
                 </div>
             </footer>
         </div>
